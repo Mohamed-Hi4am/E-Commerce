08bb4a3880d0ce62bf87c9e6e7b1d7394b2c56d0
Mohamed Hisham <mohamedhisham2003.official@gmail.com>
2025-11-23 05:04:49 +0200

    Part 03 - Service Manager

---
+++ Core/Services/Implementations/ServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Contracts;
+using Services.Abstraction.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    internal class ServiceManager(IUnitOfWork unitOfWork, IMapper mapper) : IServiceManager
+    {
+        private readonly Lazy<IProductService> _productService =
+            new Lazy<IProductService>(() => new ProductService(unitOfWork, mapper));
+
+        public IProductService ProductService => _productService.Value;
+    }
+}
