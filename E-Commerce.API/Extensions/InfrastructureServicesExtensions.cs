619cf6b91a3f523c546d55580f30f62047b2f497
Mohamed Hisham <mohamedhisham2003.official@gmail.com>
2026-02-03 00:27:34 +0200

    Identity Classes & Identity Context

--- E-Commerce.API/Extensions/InfrastructureServicesExtensions.cs
+++ E-Commerce.API/Extensions/InfrastructureServicesExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Contracts;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
+using Persistence.Identity;
 using Persistence.Repositories;
 using StackExchange.Redis;
 
@@ -17,6 +18,11 @@ namespace E_Commerce.API.Extensions
             services.AddDbContext<StoreDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddDbContext<StoreIdentityContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("IdentitySQLConnection"));
             });
 
             services.AddSingleton<IConnectionMultiplexer>( _ => ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis")!));
