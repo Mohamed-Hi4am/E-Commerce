8de55e98a4067dda4333f194b8a6b65e49fda1ed
Mohamed Hisham <mohamedhisham2003.official@gmail.com>
2026-03-22 13:00:15 +0200

    Order Mapping Profile

---
+++ Core/Services/MappingProfiles/OrderItemPictureUrlResolver.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper.Execution;
+using Domain.Entities.OrderModule;
+using Microsoft.Extensions.Configuration;
+using Shared.Dtos.OrderModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.MappingProfiles
+{
+    public class OrderItemPictureUrlResolver(IConfiguration configuration) : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+        {
+            // https://localhost:7081+ images/products/ItalianChickenMarinade.png
+
+            if (string.IsNullOrWhiteSpace(source.Product.PictureUrl))
+                return string.Empty;
+
+            // BaseUrl + source.PictureUrl
+            // https://localhost:7081+ images/products/ItalianChickenMarinade.png
+
+            return $"{configuration["BaseUrl"]} {source.Product.PictureUrl}";
+        }
+    }
+}
